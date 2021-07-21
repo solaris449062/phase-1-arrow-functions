459ced7e5c6f61968f40ebd186e929fcbd188970
Sungoh Park <spark449062@gmail.com>
2021-07-21 15:28:13 -0600

    commit tree a3f13954add6c052c1c166678b9552cf103ee0f6

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = (num1 = 2000, num2 = 100) => (num1/num2);
+
+const square = x => x*x;
+
+const add = (x, y) => x + y;
+
+console.log(divide(2000,100));
+console.log(square(2));
+console.log(add(2,3));
